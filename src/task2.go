486e2d052a03d154b782b05d6e251552e4fbdb32
Jacob Bogner <boginator55@gmail.com>
2021-12-14 12:54:52 -0500

    fix prompt for readability

--- src/task2.go
+++ src/task2.go
@@ -17,11 +17,11 @@ import (
 func Task2() {
 	var location = "assets/" //Declare a string to hold the name of the directory
 	keyboard := bufio.NewScanner(os.Stdin)
-	fmt.Println("Gob's Program: Y/N?\n?: ") //Prompt user to run program
+	fmt.Println("Run Program: Y/N?\n?: ") //Prompt user to run program
 	keyboard.Scan()
-	run := keyboard.Text() //read keyboard input
+	run := keyboard.Text()                        //read keyboard input
 	outputFile, err := os.Create("wordcount.txt") //Create a 'wordcount.txt' file to write our datamap to
-	if err != nil { //handle file creation error
+	if err != nil {                               //handle file creation error
 		log.Fatal("There was a problem creating the file. ", err)
 	}
 	defer outputFile.Close()
@@ -29,7 +29,7 @@ func Task2() {
 		fmt.Println("Brace yourself")
 		wordCount := make(chan map[string]int)
 		fileArray := getTextFiles(location) //Get the list of .txt files
-		total := 4 //Current total number of goroutines we will run
+		total := 4                          //Current total number of goroutines we will run
 		var wg sync.WaitGroup
 		wg.Add(total)
 
@@ -42,15 +42,15 @@ func Task2() {
 		wg.Wait()
 
 		writeWordCount(outputFile, datamap, "INSERTED VIA GOROUTINE") //Print the word count to the output file
-		fmt.Println("\nGob's Program: Y/N?\n?: ") //Prompt user to run program
+		fmt.Println("\nRun Program: Y/N?\n?: ")                       //Prompt user to run program
 		keyboard.Scan()
 		run = keyboard.Text() //read keyboard input
 	}
 }
 
-func wordCounterRoutine(wordCount chan<- map[string]int, fileArray []string, wg *sync.WaitGroup){
+func wordCounterRoutine(wordCount chan<- map[string]int, fileArray []string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for index := range fileArray{ //For each file in the list
+	for index := range fileArray { //For each file in the list
 		datamap := countWords(fileArray[index]) //Count the words in the file
 		wordCount <- datamap
 	}
